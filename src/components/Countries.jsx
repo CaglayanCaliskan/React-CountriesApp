24e9a0b7fa8c96b41d9b1bb53e27d59320bc3449
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-03-22 20:33:05 +0300

    filter Search bar

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,6 +1,12 @@
+import {useState} from 'react';
 import Card from './Card';
 
 function Countries({data}) {
+  const [state, setState] = useState('');
+  const filterMe = (e) => {
+    setState(e.target.value.toLowerCase());
+  };
+
   // just sort for Alphabet
   data.sort((a, b) => {
     let fa = a.name.common.toLowerCase(),
@@ -17,9 +23,16 @@ function Countries({data}) {
   return (
     <div className='container'>
       <h3>Total {data.length} Countries Listed</h3>
-      {data.map((country, index) => (
-        <Card key={index} country={country} />
-      ))}
+      <input type='text' value={state} onChange={filterMe} />
+      {data
+        .filter((item) => {
+          if (item.name.common.toLowerCase().includes(state)) {
+            return item;
+          }
+        })
+        .map((country, index) => (
+          <Card key={index} country={country} />
+        ))}
     </div>
   );
 }
